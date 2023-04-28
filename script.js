af2452e11d587c2a08decece117a15f9149afb2d
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-04-28 23:43:50 +0530

    js script added

---
+++ script.js
@@ -0,0 +1,35 @@
+const email = document.querySelector('#mail');
+const btn = document.querySelector('.btn');
+const form = document.querySelector('.notify');
+
+form.addEventListener('submit',e =>{
+    console.log('working');
+    e.preventDefault();
+
+    validate();
+
+});
+const validate = () =>{
+
+    const emailVal = email.value;
+
+    let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(emailVal===""){
+        setErrorMsg(email, "Email cannot be empty");
+    }
+    else if(emailVal.match(pattern)){
+        setErrorMsg(email,"Thanks to subscribing to our newsletter");
+    }
+    else{
+        setErrorMsg(email,"Please provide a valid email address");
+    }
+
+}
+
+function setErrorMsg(input, errormsgs){
+    const formControl = input.parentElement;
+    // console.log(formControl);
+    const msg = document.getElementById('error-msg');
+    formControl.className = "form-control error";
+    msg.innerText = errormsgs;
+}
